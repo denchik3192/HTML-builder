d92a1bdc4578698f6bbdb7828fca8b59ab779ef9
DENIS <den465341@gmail.com>
2021-11-07 22:59:03 +0300

    feat: implement copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs= require('fs');
+const path = require('path');
 
+const pathToFoloderCopy = path.join(__dirname, 'files-copy');
+const pathToFoloderFiles = path.join(__dirname, 'files');
+
+fs.mkdir(pathToFoloderCopy, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(pathToFoloderCopy, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      fs.unlink(path.join(pathToFoloderCopy, file), (err) => {
+        if (err) throw err;
+      });
+    });
+  }
+});
+
+fs.readdir(pathToFoloderFiles, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      fs.copyFile(
+        path.join(pathToFoloderFiles, file),
+        path.join(pathToFoloderCopy, file), (err) => {
+          if (err) throw err;
+        }
+      );
+    });
+  }
+  console.log('The files have been copied successfully!');
+});
